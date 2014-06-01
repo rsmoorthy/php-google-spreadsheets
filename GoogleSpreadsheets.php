3549e464450fc092d9d380fd8508ad5b3b8d22cb
Moorthy RS <rsmoorthy@gmail.com>
2014-06-01 12:23:14 +0530

    Place curl inside of GSS

--- GoogleSpreadsheets.php
+++ GoogleSpreadsheets.php
@@ -16,8 +16,153 @@
  * limitations under the License.
  */
 
-// use SimpleXMLElement;
-require_once "curl.php";
+class gcURL {
+	var $headers;
+	var $user_agent;
+	var $compression;
+	var $cookie_file;
+	var $proxy = null;
+	var $imgbuf = null;
+	var $countcaptchas = 0;
+
+	var $captcha = null;
+	var $firsturl = '';
+	var $imageurl = '';
+	var $shcarturl = '';
+	var $keywordsurl = '';
+	var $sorturl = '';
+	var $errstr = null;
+	var $ipaddr = null;
+
+	function gcURL($cookies=TRUE,$cookie='',$ipaddr=null, $compression='gzip',$proxy='') {
+		$this->headers[] = 'Accept:	text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+		$this->headers[] = 'Accept-Language: en-us,en;q=0.5';
+		$this->headers[] = 'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7';
+		$this->user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.46 Safari/535.11';
+		$this->compression=$compression;
+		$this->cookies=$cookies;
+		$this->kwcnt = 0;
+		if (!empty($this->cookies)) $this->cookie($cookie);
+	}
+	function cookie($cookie_file) {
+		if (file_exists($cookie_file)) {
+			$this->cookie_file=$cookie_file;
+		} else {
+			$this->cookie_file = "/dev/null";
+			return;
+			$fh = fopen($cookie_file,'w') or $this->error('The cookie file could not be opened. Make sure this directory has the correct permissions');
+			$this->cookie_file=$cookie_file;
+			fclose($fh);
+		}
+	}
+	function set_header($header) {
+		$this->headers[] = $header;
+	}
+	function get($url) {
+		$process = curl_init($url);
+		curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers);
+		curl_setopt($process, CURLOPT_HEADER, 0);
+		curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file);
+		// curl_setopt($process,CURLOPT_ENCODING , $this->compression);
+		curl_setopt($process, CURLOPT_TIMEOUT, 120);
+		if ($this->proxy) curl_setopt($cUrl, CURLOPT_PROXY, $this->proxy);
+		curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+		if($this->ipaddr) curl_setopt($process, CURLOPT_INTERFACE, $this->ipaddr);
+		$return = curl_exec($process);
+		if(!$return)
+			$this->errstr = curl_error($process);
+		curl_close($process);
+		return $return;
+	}
+	function post($url,$data) {
+		$process = curl_init($url);
+		curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers);
+		curl_setopt($process, CURLOPT_HEADER, 0);
+		curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file);
+		// curl_setopt($process, CURLOPT_ENCODING , $this->compression);
+		curl_setopt($process, CURLOPT_TIMEOUT, 120);
+		if ($this->proxy) curl_setopt($process, CURLOPT_PROXY, $this->proxy);
+		curl_setopt($process, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+		if($this->ipaddr) curl_setopt($process, CURLOPT_INTERFACE, $this->ipaddr);
+		curl_setopt($process, CURLOPT_POST, 1);
+		$return = curl_exec($process);
+		curl_close($process);
+		return $return;
+	}
+	function post2($url, $data) {
+		$d = array();
+		foreach($data as $k=>$v)
+			$d[] = "$k=" . urlencode($v);
+		$sdata = implode("&", $d);
+		return $this->post($url, $sdata);
+	}
+	function error($error) {
+		die("ERROR: $error\n");
+	}
+
+	function put($url,$data) {
+		$process = curl_init($url);
+		curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers);
+		curl_setopt($process, CURLOPT_HEADER, 1);
+		curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file);
+		// curl_setopt($process, CURLOPT_ENCODING , $this->compression);
+		curl_setopt($process, CURLOPT_TIMEOUT, 120);
+		if ($this->proxy) curl_setopt($process, CURLOPT_PROXY, $this->proxy);
+		curl_setopt($process, CURLOPT_PUT, 1);
+		curl_setopt($process, CURLOPT_BINARYTRANSFER, 1);
+		curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($process, CURLOPT_HEADER, false);
+		curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+		if($this->ipaddr) curl_setopt($process, CURLOPT_INTERFACE, $this->ipaddr);
+
+		$fp = fopen('php://temp/maxmemory:256000', 'w');
+		if (!$fp)
+			return false;
+		fwrite($fp, $data);
+		fseek($fp, 0); 
+		curl_setopt($process, CURLOPT_INFILE, $fp); // file pointer
+		curl_setopt($process, CURLOPT_INFILESIZE, strlen($data)); 
+
+		$return = curl_exec($process);
+		curl_close($process);
+		return $return;
+	}
+
+	function delete($url) {
+		$process = curl_init($url);
+		curl_setopt($process, CURLOPT_HTTPHEADER, $this->headers);
+		curl_setopt($process, CURLOPT_HEADER, 0);
+		curl_setopt($process, CURLOPT_USERAGENT, $this->user_agent);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEFILE, $this->cookie_file);
+		if ($this->cookies == TRUE) curl_setopt($process, CURLOPT_COOKIEJAR, $this->cookie_file);
+		// curl_setopt($process, CURLOPT_ENCODING , $this->compression);
+		curl_setopt($process, CURLOPT_TIMEOUT, 120);
+		if ($this->proxy) curl_setopt($process, CURLOPT_PROXY, $this->proxy);
+		curl_setopt($process, CURLOPT_CUSTOMREQUEST, "DELETE");
+		curl_setopt($process, CURLOPT_BINARYTRANSFER, 1);
+		curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($process, CURLOPT_HEADER, false);
+		curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+		if($this->ipaddr) curl_setopt($process, CURLOPT_INTERFACE, $this->ipaddr);
+
+		$return = curl_exec($process);
+		curl_close($process);
+		return $return;
+	}
+}
 
 class GoogleSpreadsheets
 {
@@ -46,7 +191,7 @@ class GoogleSpreadsheets
 	{
 		$data = array("Email"=>$emailid, "Passwd"=>$password, "accountType"=>"GOOGLE", 
 						"source"=>"curl", "service"=>"wise");
-		$curl = new cURL();
+		$curl = new gcURL();
 		$ret = $curl->post2("https://www.google.com/accounts/ClientLogin", $data);
 		if(preg_match("/Auth=(\S+)/", $ret, $m))
 			$this->auth = $m[1];
@@ -55,7 +200,7 @@ class GoogleSpreadsheets
 
 	public function listSpreadsheets()
 	{
-		$curl = new cURL();
+		$curl = new gcURL();
 		$curl->set_header("Authorization: GoogleLogin auth=" . $this->auth);
 		$ret = $curl->get("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
 		try {
@@ -79,7 +224,7 @@ class GoogleSpreadsheets
 
 	public function listWorksheets()
 	{
-		$curl = new cURL();
+		$curl = new gcURL();
 		$curl->set_header("Authorization: GoogleLogin auth=" . $this->auth);
 		$ret = $curl->get("https://spreadsheets.google.com/feeds/worksheets/{$this->key}/private/full");
 		try {
@@ -115,7 +260,7 @@ class GoogleSpreadsheets
 
 	public function getCellFeed()
 	{
-		$curl = new cURL();
+		$curl = new gcURL();
 		$curl->set_header("Authorization: GoogleLogin auth=" . $this->auth);
 		$ret = $curl->get("https://spreadsheets.google.com/feeds/cells/{$this->key}/{$this->ws}/private/basic");
 		try {
@@ -191,7 +336,7 @@ class GoogleSpreadsheets
 		}
 		$xml .= '</entry>';
 
-		$curl = new cURL();
+		$curl = new gcURL();
 		$curl->set_header("Authorization: GoogleLogin auth=" . $this->auth);
 		$curl->set_header("Content-Type: application/atom+xml");
 		$ret = $curl->post("https://spreadsheets.google.com/feeds/list/{$this->key}/{$this->ws}/private/full", $xml);
@@ -207,7 +352,7 @@ class GoogleSpreadsheets
 		if(!$rowId)
 			print "Unable to get rowId for $row\n";
 
-		$curl = new cURL();
+		$curl = new gcURL();
 		$curl->set_header("Authorization: GoogleLogin auth=" . $this->auth);
 		$curl->set_header("If-Match: *");
 		$ret = $curl->delete("https://spreadsheets.google.com/feeds/list/{$this->key}/{$this->ws}/private/full/$rowId", $xml);
@@ -216,7 +361,7 @@ class GoogleSpreadsheets
 
 	public function getRowIds()
 	{
-		$curl = new cURL();
+		$curl = new gcURL();
 		$curl->set_header("Authorization: GoogleLogin auth=" . $this->auth);
 		$ret = $curl->get("https://spreadsheets.google.com/feeds/list/{$this->key}/{$this->ws}/private/basic");
 		try {
@@ -273,7 +418,7 @@ EOD;
 
 		$xml = $header . $entries . "</feed>";
 
-		$curl = new cURL();
+		$curl = new gcURL();
 		$curl->set_header("Authorization: GoogleLogin auth=" . $this->auth);
 		$curl->set_header("If-Match: *");
 		$ret = $curl->post("https://spreadsheets.google.com/feeds/cells/{$this->key}/{$this->ws}/private/full/batch", $xml);
